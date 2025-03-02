6e389bd2c64c9209c600ce112daae34b6004f599
Alexandre <josealexandre2005@outlook.com>
2025-03-01 21:59:59 -0300

    Placing pieces on the board

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,23 +1,32 @@
 package Chess;
 
+import Chess.pieces.King;
+import Chess.pieces.Rook;
 import bordgame.Board;
+import bordgame.Position;
 
 public class ChessMatch {
 	private Board board;
-	
-	public ChessMatch () {
-		board = new Board(8,8);
+
+	public ChessMatch() {
+		board = new Board(8, 8);
+		initialSetup();
 	}
-	
-	
+
 	public ChessPiece[][] getPieces() {
-		ChessPiece[][]  mat = new ChessPiece [board.getRows()] [board.getColumns()];
-		for(int i=0;i<board.getRows();i++) {
-			for(int j=0;j<board.getColumns();j++) {
-				mat[i][j] = (ChessPiece)board.pieces(i, j);
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.pieces(i, j);
 			}
 		}
 		return mat;
+	}
+
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+		board.placePiece(new King(board,Color.BLACK), new Position(7,4));
+		board.placePiece(new King(board, Color.WHITE), new Position(0,4));
 	}
 
 }
--- src/bordgame/Board.java
+++ src/bordgame/Board.java
@@ -29,6 +29,11 @@ public class Board {
 		return pieces[position.getRow()] [position.getColum()];
 	}
 	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColum()] = piece;
+		piece.position = position;
+	}
+	
 	
 	
 	
