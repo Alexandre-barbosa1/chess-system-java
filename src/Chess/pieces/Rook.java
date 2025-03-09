d9f592ca8ffa901cd19ba12148c6d69708e28b87
Alexandre <josealexandre2005@outlook.com>
2025-03-08 22:52:07 -0300

    Implementing possible moves of Rook

--- src/Chess/pieces/Rook.java
+++ src/Chess/pieces/Rook.java
@@ -3,24 +3,64 @@ package Chess.pieces;
 import Chess.ChessPiece;
 import Chess.Color;
 import bordgame.Board;
-
+import bordgame.Position;
 
 public class Rook extends ChessPiece {
 
-	
-	
 	public Rook(Board board, Color color) {
 		super(board, color);
 	}
 
 	@Override
-	
+
 	public String toString() {
 		return "R";
 	}
+
 	@Override
 	public boolean[][] posibleMoves() {
-		boolean mat [][] = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		boolean mat[][] = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0, 0);
+		p.setValues(position.getRow() - 1, position.getColum());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+			p.setRow(p.getRow() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOponnentPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+		}
+		
+		// left
+		p.setValues(position.getRow(), position.getColum() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+			p.setColum(p.getColum() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOponnentPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+		}
+		
+		// right
+		p.setValues(position.getRow(), position.getColum() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+			p.setColum(p.getColum() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOponnentPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+		}
+		
+		// below
+		p.setValues(position.getRow() + 1, position.getColum());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOponnentPiece(p)) {
+			mat[p.getRow()][p.getColum()] = true;
+		}
+		
+
 		return mat;
 	}
 
--- src/bordgame/Piece.java
+++ src/bordgame/Piece.java
@@ -4,29 +4,31 @@ public abstract class Piece {
 
 	protected Position position;
 	private Board board;
+
 	public Piece(Board board) {
 		this.board = board;
 	}
+
 	protected Board getBoard() {
 		return board;
 	}
-	
-	public abstract boolean [][] posibleMoves();
-	
+
+	public abstract boolean[][] posibleMoves();
+
 	public boolean posibleMove(Position position) {
-		return posibleMoves() [position.getRow()][position.getColum()];
+		return posibleMoves()[position.getRow()][position.getColum()];
 	}
+
 	public boolean isThereAnyPossibleMove() {
 		boolean mat[][] = posibleMoves();
-		for(int i=0;i<mat.length;i++) {
-			for(int j=0;j<mat.length;j++) {
-				if(mat[i][j]) {
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
 					return true;
 				}
 			}
 		}
-			return false;
+		return false;
 	}
-	
-	
+
 }
